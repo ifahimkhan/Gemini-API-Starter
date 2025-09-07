6fb6cf5c469119a20301e4acb51d72d3cb4fa483
Parth Acharya <1008parth@gmail.com>
2025-09-07 16:35:36 +0530

    Adds ChatMessage class to encapsulate messages

---
+++ app/src/main/java/com/fahim/geminiapistarter/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.fahim.geminiapistarter;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "chat_messages")
+public class ChatMessage {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String userMessage;
+    public String aiResponse;
+
+    public ChatMessage(String userMessage, String aiResponse) {
+        this.userMessage = userMessage;
+        this.aiResponse = aiResponse;
+    }
+}
