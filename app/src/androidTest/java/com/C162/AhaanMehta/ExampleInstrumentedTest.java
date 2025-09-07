64d345972017002086397d5a82114f83fa48d93c
Ahaan Mehta <mehtaahaan@gmail.com>
2025-09-07 23:18:30 +0530

    Your commit message describing changes

--- app/src/androidTest/java/com/C162/AhaanMehta/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/C162/AhaanMehta/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.fahim.geminiapistarter", appContext.getPackageName());
+        assertEquals("com.C162.AhaanMehta", appContext.getPackageName());
     }
 }
