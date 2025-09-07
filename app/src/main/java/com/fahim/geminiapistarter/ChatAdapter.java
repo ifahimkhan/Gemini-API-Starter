557a974f85e3b2a880244fb91f5aa294c5d97cfc
Nikhil-Dsouza <nikhilrd21@gmail.com>
2025-09-08 00:49:31 +0530

    Update ChatAdapter.java

--- app/src/main/java/com/fahim/geminiapistarter/ChatAdapter.java
+++ app/src/main/java/com/fahim/geminiapistarter/ChatAdapter.java
@@ -1,1 +1,53 @@
+package com.fahim.geminiapistarter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    private final List<ChatMessage> messages;
+
+    public ChatAdapter(List<ChatMessage> messages) {
+        this.messages = messages;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_chat, parent, false);
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        ChatMessage message = messages.get(position);
+        holder.messageText.setText(message.getMessage());
+        holder.avatarImage.setImageResource(message.isUser() ? R.drawable.baseline_person_24 : R.drawable.twotone_computer_24);
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    static class ChatViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+        ImageView avatarImage;
+
+        ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.message);
+            avatarImage = itemView.findViewById(R.id.avatar);
+        }
+    }
+}
 
