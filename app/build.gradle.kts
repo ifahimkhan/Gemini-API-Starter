da638725e6b4dc6fbc4ab83eba3f1d7cc638ce55
ashmit <svvgulhane@gmail.com>
2025-09-08 23:49:28 +0530

    final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.C016.Aanchal"
+        applicationId = "com.C035.Sukhada"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
